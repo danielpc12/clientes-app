55c9a44c3fe3975cc3452095a26b51583adfe49a
danielpc12 <danielpiedrahita123@hotmail.com>
2022-05-17 21:16:00 -0500

    Creación proyecto Angular y creación componentes

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,8 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+    {id: 1, nombre: 'Juan', apellido: 'Perez', createAt: '2022-05-16', email: 'juan.perez@prueba.com'},
+    {id: 2, nombre: 'Pedro', apellido: 'Perez', createAt: '2022-05-16', email: 'pedro.perez@prueba.com'},
+    {id: 3, nombre: 'Maria', apellido: 'Perez', createAt: '2022-05-16', email: 'pedro.perez@prueba.com'},
+    {id: 4, nombre: 'Jose', apellido: 'Perez', createAt: '2022-05-16', email: 'jose.perez@prueba.com'}
+  ]
